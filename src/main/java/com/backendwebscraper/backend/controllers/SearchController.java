42c6b098ef8fe2f5a28d020c09d67b51cb113e32
Christian Cachero <Ch_canchero@live.com.ar>
2022-10-01 18:36:18 -0300

    Poniendole el nombre normal al controlador

--- src/main/java/com/backendwebscraper/backend/controllers/SearchController.java
+++ src/main/java/com/backendwebscraper/backend/controllers/SearchController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-public class IndexController {
+public class SearchController {
 
     @Autowired
     private SearchService service;
